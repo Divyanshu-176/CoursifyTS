c16f372585ff1593e1df8aa24c268a89e4c7fe1e
Divyanshu-176 <divyanshujain176@gmail.com>
2025-09-25 22:19:44 +0530

    Project Completed

--- dist/routes/course.js
+++ dist/routes/course.js
@@ -1,4 +1,33 @@
 import { Router } from "express";
-import { adminModel, courseModel, purchaseModel, userModel } from "../database/db.js";
+import { courseModel, purchaseModel } from "../database/db.js";
+import { userMiddleware } from "../middlewares/userMiddleware.js";
 export const courseRouter = Router();
+courseRouter.post("/purchase", userMiddleware, async (req, res) => {
+    const userId = req.userId;
+    const { courseId } = req.body;
+    if (!courseId) {
+        throw new Error("Provide CourseID!");
+    }
+    //here u also neeed to check that the user has actually paid the price
+    try {
+        await purchaseModel.create({
+            userId,
+            courseId
+        });
+        res.json({
+            msg: "user course purchase success"
+        });
+    }
+    catch (error) {
+        res.status(403).json({
+            msg: `error: ${error}`
+        });
+    }
+});
+courseRouter.get("/preview", async (req, res) => {
+    const courses = await courseModel.find({});
+    res.json({
+        courses: courses
+    });
+});
 //# sourceMappingURL=course.js.map
--- src/routes/course.ts
+++ src/routes/course.ts
@@ -1,7 +1,47 @@
-import { Router } from "express";
-import { adminModel, courseModel, purchaseModel, userModel } from "../database/db.js";
+import { Router, type Request, type Response } from "express";
+import {  courseModel, purchaseModel } from "../database/db.js";
 
 
-
+import { userMiddleware } from "../middlewares/userMiddleware.js";
 
 export const courseRouter = Router()
+
+
+interface AuthenticatedRequest extends Request{
+    userId?:string
+}
+
+courseRouter.post("/purchase", userMiddleware, async(req: AuthenticatedRequest,res:Response):Promise<void>=>{
+
+    const userId = req.userId;
+    const {courseId} = req.body
+    if(!courseId){throw new Error ("Provide CourseID!")}
+
+    //here u also neeed to check that the user has actually paid the price
+    try {
+        await purchaseModel.create({
+            userId,
+            courseId
+        })
+
+        res.json({
+        msg:"user course purchase success"
+    })
+    } catch (error) {
+        res.status(403).json({
+            msg:`error: ${error}`
+        })
+    }
+    
+})
+ 
+
+courseRouter.get("/preview", async (req:Request,res:Response)=>{
+    const courses = await courseModel.find({
+
+    })
+
+    res.json({
+        courses:courses
+    })
+})
